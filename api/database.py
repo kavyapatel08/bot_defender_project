66d63333dea90c5a7337ee13de4685f72ae14eb0
Kavya <kavyapatel8131@gmail.com>
2026-05-02 11:33:36 +0530

    Updated database URL to Supabase

--- api/database.py
+++ api/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/bot_defender_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:#RadheKrishn02@db.hejebfcojxomnisetfri.supabase.co:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
